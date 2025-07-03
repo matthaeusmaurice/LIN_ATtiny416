0cdab84aced29296d6256f44923bcb7e049ac7ee
Maurice Matthaeus <ext_mmt@lem.com>
2025-07-03 10:48:34 +0900

    initial commit

---
+++ LIN_ATtiny416.c
@@ -0,0 +1,13 @@
+/*
+ * File:   LIN_ATtiny416.c
+ * Author: ext_mmt
+ *
+ * Created on 2025/07/03, 10:40
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=LIN_ATtiny416.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/LIN_ATtiny416.X.production.hex
